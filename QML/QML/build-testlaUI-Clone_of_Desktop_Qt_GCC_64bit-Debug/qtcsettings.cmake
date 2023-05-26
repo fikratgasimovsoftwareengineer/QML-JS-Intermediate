9bf00a5c8b2eb686292ce49e21ad2c889d82b9df
Fikrat <fikretqasimov95@gmail.com>
2023-05-26 12:54:10 +0200

    Tesla

--- QML/QML/build-testlaUI-Clone_of_Desktop_Qt_GCC_64bit-Debug/qtcsettings.cmake
+++ QML/QML/build-testlaUI-Clone_of_Desktop_Qt_GCC_64bit-Debug/qtcsettings.cmake
@@ -1,4 +1,2 @@
 # This file is managed by Qt Creator, do not edit!
 
-set("QT_QMAKE_EXECUTABLE" "/home/fikrat/Qt/5.15.2/gcc_64/bin/qmake" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "/home/fikrat/Qt/5.15.2/gcc_64;" CACHE "PATH" "" FORCE)
